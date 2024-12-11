699fd174c2618ab4c4fd28225f19dbc6c28fdffa
John Biddiscombe <biddisco@cscs.ch>
2024-12-11 11:42:14 +0100

    Tweak glibmm for ardour DAW build

--- var/spack/repos/builtin/packages/glibmm/package.py
+++ var/spack/repos/builtin/packages/glibmm/package.py
@@ -10,16 +10,19 @@ class Glibmm(AutotoolsPackage):
     """Glibmm is a C++ wrapper for the glib library."""
 
     homepage = "https://gitlab.gnome.org/GNOME/glibmm"
-    url = "https://download-fallback.gnome.org/sources/glibmm/2.70/glibmm-2.70.0.tar.xz"
-
+    url = "https://download.gnome.org/sources/glibmm/2.82/glibmm-2.82.0.tar.xz"
     license("LGPL-2.1-or-later")
 
-    # version('2.70.0', sha256='8008fd8aeddcc867a3f97f113de625f6e96ef98cf7860379813a9c0feffdb520')
+    version('2.82.0', sha256='38684cff317273615c67b8fa9806f16299d51e5506d9b909bae15b589fa99cb6')
     version("2.19.3", sha256="23958368535c19188b1241c4615dcf1f35e80e0922a04236bb9247dcd8fe0a2b")
     version("2.16.0", sha256="99795b9c6e58e490df740a113408092bf47a928427cbf178d77c35adcb6a57a3")
     version("2.4.8", sha256="78b97bfa1d001cc7b398f76bf09005ba55b45ae20780b297947a1a71c4f07e1f")
 
     depends_on("cxx", type="build")  # generated
+
+    depends_on("autoconf", type="build", when="@2.82.0 build_system=autotools")
+    depends_on("automake", type="build", when="@2.82.0 build_system=autotools")
+    depends_on("libtool", type="build", when="@2.82.0 build_system=autotools")
 
     depends_on("libsigcpp")
     # https://libsigcplusplus.github.io/libsigcplusplus/index.html
